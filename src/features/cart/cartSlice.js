e800bf32dee66c639e05b97e85dc7b184c6085ee
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2023-11-23 16:29:38 +0300

    update user slice

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -1,39 +1,36 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 const initialState = {
-  cart: [
-    {
-      pizzaId: 12,
-      name: 'Test pizza',
-      quantity: 2,
-      unitPrice: 16,
-      totalPrice: 32,
-    },
-  ],
+  cart: [],
 };
 
 const cartSlice = createSlice({
   name: 'cart',
   initialState,
-
   reducers: {
     addItem(state, action) {
+      // payload = newItem
       state.cart.push(action.payload);
     },
     deleteItem(state, action) {
-      state.cart.filter((item) => item.pizzaId !== action.payload);
+      // payload = pizzaId
+      state.cart = state.cart.filter((item) => item.pizzaId !== action.payload);
     },
     increaseItemQuantity(state, action) {
+      // payload = pizzaId
       const item = state.cart.find((item) => item.pizzaId === action.payload);
 
       item.quantity++;
       item.totalPrice = item.quantity * item.unitPrice;
     },
     decreaseItemQuantity(state, action) {
+      // payload = pizzaId
       const item = state.cart.find((item) => item.pizzaId === action.payload);
 
       item.quantity--;
       item.totalPrice = item.quantity * item.unitPrice;
+
+      if (item.quantity === 0) cartSlice.caseReducers.deleteItem(state, action);
     },
     clearCart(state) {
       state.cart = [];
@@ -43,10 +40,10 @@ const cartSlice = createSlice({
 
 export const {
   addItem,
-  clearCart,
-  decreaseItemQuantity,
   deleteItem,
   increaseItemQuantity,
+  decreaseItemQuantity,
+  clearCart,
 } = cartSlice.actions;
 
 export default cartSlice.reducer;
@@ -61,3 +58,5 @@ export const getTotalCartPrice = (state) =>
 
 export const getCurrentQuantityById = (id) => (state) =>
   state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
+
+// 'reselect'
